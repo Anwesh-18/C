b2f725a059b3a3d4f3ad50eb20e79c2ef9b99691
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-11-30 12:55:27 +0530

    Scheduling Done!!

--- schedule.c
+++ schedule.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 // Designing the structure of Process 
-typedef struct process{
+typedef struct{
     int pid;
     int arrival;
     int burst;
@@ -12,114 +12,459 @@ typedef struct process{
     int completion;
     int waiting;
     int turnaround;
-}P;
-P p[100];
-typedef struct GanttChart{
+    int index;
+}Process;
+Process* p = NULL;
+int n = 0;
+
+typedef struct{
     int pid;
     int start;
     int end;
-}G;
-G gc[100];
-int gc_index = 0;
-void printGantt(G gc[],int n){
-    for(int i=0;i<n;i++){
-        printf("| P%d (%d - %d) ", gc[i].pid, gc[i].start, gc[i].end);
+}Gantt;
+Gantt *gc = NULL;
+int gc_size = 0;
+
+typedef struct {
+    int pid;
+    int arrival;
+    int remaining;
+    int burst;
+    int priority;
+    int index;
+    int key;
+}HeapNode;
+HeapNode *heap = NULL;
+int heapSize = 0;
+
+void printGantt() {
+    for(int i=0;i<gc_size;i++){
+        if (gc[i].pid == -1) {
+            printf("| %s (%d - %d) ", "IDLE", gc[i].start, gc[i].end);
+        }else {
+            printf("| P%d (%d - %d) ", gc[i].pid, gc[i].start, gc[i].end);
+        }
     }
     printf("|\n");
 }
-typedef struct{
-    int pid;
-    int arrival;
-    int burst;
-}Node;
-Node heap[100];
-int heapSize = 0;
-// Node swap logic
-void swap(Node *a, Node *b){
-    Node temp = *a;
-    *a = *b;
-    *b = temp;
-}
-void heapPush(Node val){
-    heapSize++;
-    heap[heapSize] = val;
-    int i = heapSize;
-    while(i>1 && heap[i].burst < heap[i/2].burst){
-        swap(&heap[i],&heap[i/2]);
-        i /= 2;
-    }
-}
-Node heapPop(){
-    Node minVal = heap[1];
-    heap[1] = heap[heapSize];
-    heapSize--;
+void addGantt(int pid,int start,int end) {
+    gc = realloc(gc,(gc_size+1)*sizeof(Gantt));
 
-    int i=1;
-    while(1){
-        int left = 2*i;
-        int right = 2*i+1;
-        int smallest = i;
+    gc[gc_size].pid = pid;
+    gc[gc_size].start = start;
+    gc[gc_size].end = end;
 
-        if(left < heapSize && heap[smallest] > heap[smallest]){
-            smallest = left;
-        }
-        if(right < heapSize && heap[smallest] > heap[smallest]){
-            smallest = right;
-        }
-
-        if(smallest != i){
-            swap(&heap[i],&heap[smallest]);
-            i = smallest;
-        }
-        else break;
-    }
+    gc_size++;
 }
-void printDetails(P p[],int n){
-    int time=0;
-    int wt = 0;
-    int tat = 0;
-    for(int i=0;i<n;i++){
-        // Assigning pid
-        gc[i].pid = p[i].pid;
-        // calculating times
-        // If CPU is idle, jump time
-        if (time < p[i].arrival)
-            time = p[i].arrival;
-        p[i].start = time;
-        time += p[i].burst;
-        p[i].completion = time;
-        p[i].waiting = p[i].start - p[i].arrival;
-        p[i].turnaround = p[i].completion - p[i].arrival;
-        gc[i].pid = p[i].pid;
-        gc[i].start = p[i].start;
-        gc[i].end = p[i].completion;
-        wt += p[i].waiting;
-        tat += p[i].turnaround;
-    }
-    printGantt(gc,n);
-    printf("The average waiting time: %f\n",(float)wt/n);
-    printf("The average turn around time: %f\n",(float)tat/n);
-}
-void FCFS(P p[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if (p[i].arrival > p[j].arrival){
-                P temp = p[i];
+void sortByArrivalTime() {
+    for (int i=0;i<n-1;i++) {
+        for (int j=i+1;j<n;j++) {
+            if (p[i].arrival > p[j].arrival) {
+                Process temp = p[i];
                 p[i] = p[j];
                 p[j] = temp;
             }
         }
     }
-    printDetails(p,n);
 }
-void SJF(P p[],int n){
-    time = 0;
-    completed = 0;
+
+void swap(HeapNode* a,HeapNode* b) {
+    HeapNode temp = *a;
+    *a = *b;
+    *b = temp;
 }
-void main(){
-    int n;
+void heapify(int i) {
+    int left = 2*i+1;
+    int right = 2*i+2;
+    int smallest = i;
+
+    if (left<heapSize && heap[smallest].key > heap[left].key) {
+        smallest = left;
+    }
+    if (right<heapSize && heap[smallest].key > heap[right].key) {
+        smallest = right;
+    }
+    if (smallest != i) {
+        swap(&heap[i],&heap[smallest]);
+        heapify(smallest);
+    }
+}
+void heapPush(HeapNode node) {
+    heap = realloc(heap,(heapSize+1)*sizeof(HeapNode));
+    heap[heapSize] = node;
+    int i = heapSize;
+    heapSize++;
+
+    while (i>0 && heap[i].key < heap[(i-1)/2].key) {
+        swap(&heap[i],&heap[(i-1)/2]);
+        i = (i-1)/2;
+    }
+}
+HeapNode heapPop() {
+    HeapNode res = heap[0];
+    heap[0] = heap[heapSize-1];
+    heapSize--;
+
+    heapify(0);
+    return res;
+}
+// FCFS SCHEDULING
+void FCFS(){
+    gc_size = 0;
+    sortByArrivalTime();
+    int time = 0;
+    for (int i=0;i<n;i++) {
+        if (p[i].arrival > time) {
+            addGantt(-1,time,p[i].arrival);
+            time = p[i].arrival;
+        }
+        p[i].start = time;
+        p[i].waiting = p[i].start - p[i].arrival;
+        time += p[i].burst;
+        p[i].completion = time;
+        p[i].turnaround = p[i].completion - p[i].arrival;
+        addGantt(p[i].pid,p[i].start,p[i].completion);
+    }
+    printGantt();
+}
+// SJF SCHEDULING
+void SJF() {
+    gc_size = 0;
+    sortByArrivalTime();
+    int time = 0;
+    int completed = 0;
+    int i = 0;
+    while (completed < n) {
+        while (i<n && p[i].arrival <= time) {
+            HeapNode node;
+            node.pid = p[i].pid;
+            node.arrival = p[i].arrival;
+            node.remaining = p[i].remaining;
+            node.burst = p[i].burst;
+            node.priority = p[i].priority;
+            node.index = p[i].index;
+
+            node.key = node.burst;
+            heapPush(node);
+            i++;
+        }
+        if (heapSize == 0) {
+            addGantt(-1,time,p[i].arrival);
+            time = p[i].arrival;
+            continue;
+        }
+        HeapNode h = heapPop();
+        Process curr = p[h.index];
+        curr.start = time;
+        time += curr.burst;
+        curr.completion = time;
+        curr.turnaround = curr.completion - curr.arrival;
+        curr.waiting = curr.start - curr.arrival;
+        p[curr.index] = curr;
+        addGantt(curr.pid,curr.start,curr.completion);
+        completed ++;
+    }
+    printGantt();
+}
+// SRTF SCHEDULING
+void SRTF() {
+    gc_size = 0;
+    sortByArrivalTime();
+    heapSize = 0;
+    free(heap);
+    heap = NULL;
+
+    int time = 0;
+    int completed = 0;
+    int i = 0;
+
+    int prev_pid = -1;
+    int blocktStart = 0;
+
+    for (int i=0;i<n;i++) {
+        p[i].start = -1;
+    }
+
+    while (completed < n) {
+        while (i<n && p[i].arrival <= time) {
+            HeapNode node;
+            node.pid = p[i].pid;
+            node.arrival = p[i].arrival;
+            node.remaining = p[i].remaining;
+            node.burst = p[i].burst;
+            node.priority = p[i].priority;
+            node.index = p[i].index;
+
+            node.key = p[i].remaining;
+            heapPush(node);
+            i++;
+        }
+        if (heapSize == 0) {
+            if (prev_pid != -1) {
+                addGantt(prev_pid,blocktStart,time);
+            }
+            prev_pid = -1;
+            time ++;
+            continue;
+        }
+        HeapNode h = heapPop();
+        Process curr = p[h.index];
+
+        if (curr.pid != prev_pid) {
+            if (prev_pid != -1) {
+                addGantt(prev_pid,blocktStart,time);
+            }
+            blocktStart = time;
+        }
+        curr.remaining --;
+        time ++;
+        if (curr.remaining == 0) {
+            curr.completion = time;
+            curr.turnaround = curr.completion - curr.arrival;
+            curr.waiting = curr.turnaround - curr.burst;
+            p[curr.index] = curr;
+            addGantt(curr.pid,blocktStart,time);
+            completed ++;
+            prev_pid = -1;
+        }else {
+            p[curr.index] = curr;
+            h.remaining  = curr.remaining;
+            h.key = curr.remaining;
+            heapPush(h);
+
+            prev_pid = curr.pid;
+        }
+    }
+    printGantt();
+}
+// PRIORITY SCHEDULING
+void PRIORITY_P() {
+    gc_size = 0;
+    sortByArrivalTime();
+    heapSize = 0;
+    free(heap);
+    heap = NULL;
+    int time = 0;
+
+    int completed = 0;
+    int i = 0;
+
+    int prev_pid = -1;
+    int blockStart = 0;
+
+    for (int i=0;i<n;i++) {
+        p[i].start = -1;
+    }
+
+    while (completed < n) {
+        while (i < n && p[i].arrival <= time) {
+            HeapNode node;
+            node.pid = p[i].pid;
+            node.arrival = p[i].arrival;
+            node.remaining = p[i].remaining;
+            node.burst = p[i].burst;
+            node.priority = p[i].priority;
+            node.index = p[i].index;
+
+            node.key = p[i].priority;
+            heapPush(node);
+            i++;
+        }
+        if (heapSize == 0) {
+            if (prev_pid != -1) {
+                addGantt(prev_pid, blockStart, time);
+            }
+            prev_pid = -1;
+            time++;
+            continue;
+        }
+        HeapNode h = heapPop();
+        Process curr = p[h.index];
+
+        if (curr.start == -1)
+            curr.start = time;
+
+        if (curr.pid != prev_pid) {
+            if (prev_pid != -1) {
+                addGantt(prev_pid, blockStart, time);
+            }
+            blockStart = time;
+        }
+        time++;
+        curr.remaining --;
+        if (curr.remaining == 0) {
+            curr.completion = time;
+            curr.turnaround = curr.completion - curr.arrival;
+            curr.waiting = curr.turnaround - curr.burst;
+
+            p[curr.index] = curr;
+            addGantt(curr.pid, blockStart, time);
+
+            prev_pid = -1;
+            completed++;
+        }else {
+            p[curr.index] = curr;
+            h.remaining  = curr.remaining;
+            h.key = curr.priority;
+            heapPush(h);
+            prev_pid = curr.pid;
+        }
+    }
+    printGantt();
+}
+void PRIORITY_N() {
+    gc_size = 0;
+    sortByArrivalTime();
+    heapSize = 0;
+    free(heap);
+    heap = NULL;
+    int time = 0;
+    int completed = 0;
+    int i = 0;
+
+    while (completed < n){
+        while (i < n && p[i].arrival <= time) {
+            HeapNode node;
+            node.pid = p[i].pid;
+            node.arrival = p[i].arrival;
+            node.remaining = p[i].remaining;
+            node.burst = p[i].burst;
+            node.priority = p[i].priority;
+            node.index = p[i].index;
+
+            node.key = p[i].priority;
+            heapPush(node);
+            i++;
+        }
+
+        if (heapSize == 0) {
+            addGantt(-1,time,p[i].arrival);
+            time = p[i].arrival;
+            continue;
+        }
+        HeapNode h = heapPop();
+        Process curr = p[h.index];
+        curr.start = time;
+        time += curr.burst;
+        curr.completion = time;
+        curr.turnaround = curr.completion - curr.arrival;
+        curr.waiting = curr.turnaround- curr.burst;
+        p[curr.index] = curr;
+        addGantt(curr.pid,curr.start,curr.completion);
+        completed ++;
+    }
+    printGantt();
+}
+int *queue = NULL;
+int q_front = 0, q_rear = 0, q_size = 0;
+
+void initQueue() {
+    free(queue);
+    queue = NULL;
+    q_front = q_rear = q_size = 0;
+}
+
+int isQueueEmpty() {
+    return q_size == 0;
+}
+
+void pushQueue(int idx) {
+    queue = realloc(queue, (q_size + 1) * sizeof(int));
+    queue[q_rear] = idx;
+    q_rear++;
+    q_size++;
+}
+int popQueue() {
+    int idx = queue[q_front];
+    q_front++;
+    q_size--;
+
+    if (q_front == q_rear) {
+        free(queue);
+        queue = NULL;
+        q_front = q_rear = q_size = 0;
+    }
+    return idx;
+}
+// RR Scheduling
+void RR(int tc) {
+    // free(queue);
+    initQueue();
+    gc_size = 0;
+    sortByArrivalTime();
+
+    for (int k = 0; k < n; k++) {
+        p[k].remaining = p[k].burst;
+    }
+
+    int time = 0;
+    int blockStart = 0;
+    int completed = 0;
+    int i = 0;
+    int prev_pid = -1;
+
+    while (completed < n) {
+        while (i<n && p[i].arrival <= time) {
+            pushQueue(p[i].index);
+            i++;
+        }
+        if (isQueueEmpty()) {
+            if (i<n) {
+                addGantt(-1,time,p[i].arrival);
+                time = p[i].arrival;
+                continue;
+            }else {
+                break;
+            }
+        }
+
+        int idx = popQueue();
+        Process curr = p[idx];
+
+        if (curr.pid != prev_pid) {
+            if (prev_pid != -1) {
+                addGantt(prev_pid,blockStart,time);
+            }
+            blockStart = time;
+        }
+        int runTime = (curr.remaining<tc) ? curr.remaining : tc;
+        for (int t = 0; t < runTime; t++) {
+            time++;
+            curr.remaining--;
+
+            while (i < n && p[i].arrival <= time) {
+                pushQueue(p[i].index);
+                i++;
+            }
+
+            if (curr.remaining == 0)
+                break;
+        }
+        if (curr.remaining == 0) {
+            curr.completion = time;
+            curr.turnaround = curr.completion - curr.arrival;
+            curr.waiting = curr.turnaround - curr.burst;
+
+            addGantt(curr.pid, blockStart, time);
+            prev_pid = -1;
+            completed++;
+        }
+        else {
+            // not completed → requeue
+            pushQueue(curr.index);
+            prev_pid = curr.pid;
+        }
+        p[curr.index] = curr;
+    }
+    printGantt();
+}
+
+int main(){
     printf("Enter the number of processes: ");
     scanf("%d",&n);
+
+    p = calloc(n,sizeof(Process));
+
     printf("-----Enter the process details-----\n");
     for(int i=0;i<n;i++){
         printf("Enter the process id: ");
@@ -130,28 +475,44 @@ void main(){
         scanf("%d",&p[i].burst);
         printf("Enter the process's priority: ");
         scanf("%d",&p[i].priority);
+        p[i].index = i;
+        p[i].remaining = p[i].burst;
     }
     int choice = 1;
     while(choice){
         printf("-----Operations-----\n");
         printf("1.FCFS\n2.SJF\n3.SRTF\n4.PRIORITY\n5.RR\n");
         printf("Enter your choice: ");
-        int ch;
+        int ch,x;
         scanf("%d",&ch);
         switch(ch){
-            case 1: FCFS(p,n);
+            case 1: FCFS();
                     break;
-                // case 2: SJF(p,n);
-                //         break;
-                // case 3: SRTF(p,n);
-                //         break;
-                // case 4: PRIORITY(p,n);
-                //         break;
-                // case 5: RR(p,n);
-                //         break;
+            case 2: SJF();
+                    break;
+            case 3: SRTF();
+                    break;
+            case 4: printf("Which one:\n1.Preemptive\n2.Non-preemptive\nEnter your choice: ");
+                    scanf("%d",&x);
+                    switch(x) {
+                        case 1: PRIORITY_P();
+                                break;
+                        case 2: PRIORITY_N();
+                                break;
+                        default: printf("Wrong choice!! Try again\n");
+                    }
+                    break;
+            case 5: printf("Enter the timeQuantum: ");
+                    scanf("%d",&x);
+                    RR(x);
+                    break;
             default: printf("Invalid Input!!\n");
         }
         printf("Want to continue?(0,1): ");
         scanf("%d",&choice);
     }
+    printf("Thank You!!\n");
+    free(p);
+    free(gc);
+    return 0;
 }
